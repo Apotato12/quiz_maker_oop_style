79d6ea6cff912925307f9a25f15bb815fc2daaf5
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-05-21 11:22:21 +0800

    added_file_maker_class

--- quiz_creator_oop.py
+++ quiz_creator_oop.py
@@ -3,3 +3,10 @@ import os
 class file_name():
     def file_name(self, file_name = "quiz.txt"):
      self.file_name = file_name
+    
+class file_maker():
+   def make_file(self, file_name):
+      if not os.path.exists(file_name):
+         open(file_name, 'w').close
+      else:
+         print(f"file {file_name} already exists")
