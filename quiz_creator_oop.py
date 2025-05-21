b0bc733fd357dc1ff439d88a73c808e259ab287f
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-05-21 11:06:10 +0800

    added_more_steps_to_the_pseudocode

--- quiz_creator_oop.py
+++ quiz_creator_oop.py
@@ -1,1 +1,2 @@
-#use the previous code as a base and transfrom it into object oriented progr
+#use the previous code as a base and transfrom it into object oriented program
+#use the 4 basic principles discussed in the previous lesson : inheritance, encapsulation, polymorphism and abstraction
