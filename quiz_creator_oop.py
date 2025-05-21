319081ad4be7df0f934cff8463a9ee9b8d02bb96
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-05-21 11:10:25 +0800

    added_class_for_file_name

--- quiz_creator_oop.py
+++ quiz_creator_oop.py
@@ -1,2 +1,2 @@
-#use the previous code as a base and transfrom it into object oriented program
-#use the 4 basic principles discussed in the previous lesson : inheritance, encapsulation, polymorphism and abstraction
+class file_name:
+    file_name = "quiz.txt"
