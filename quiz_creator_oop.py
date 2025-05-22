bbebca5bd0a7a0b16038410240258787dfa564d4
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-05-23 01:41:39 +0800

    changed_to_pascal_case

--- quiz_creator_oop.py
+++ quiz_creator_oop.py
@@ -1,12 +1,20 @@
 import os
 
-class file_name():
+class Filename:
     def file_name(self, file_name = "quiz.txt"):
      self.file_name = file_name
     
-class file_maker():
+class FileMaker:
    def make_file(self, file_name):
       if not os.path.exists(file_name):
          open(file_name, 'w').close
       else:
          print(f"file {file_name} already exists")
+
+def main_body():
+   filename_obj = Filename()
+   file_maker = FileMaker(filename_obj)
+   file_maker.make_file()
+
+
+main_body()
