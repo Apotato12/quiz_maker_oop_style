aeb62ea8dae4aa0850731bf6dd08765265e3bfb9
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-05-23 02:18:08 +0800

    moved_the_continue_or_exit_to_quiz_launcher

--- quiz_launcher.py
+++ quiz_launcher.py
@@ -9,12 +9,17 @@ class QuizLauncher(Filename, FileMaker, QuestionParts, QuestionWriter):
         self.make_file()   
 
 
-    def run(self):
+def run(self):
         while True:
             question = self.ask_question()
             answers = self.get_answers()
             correct_answer = self.get_correct_answer()
             self.save_question_to_file(question, answers, correct_answer)
+
+            continue_or_quit = ContinueOrQuit()
+            if continue_or_quit.another_question.lower() != "y":
+                print("Exiting the program.")
+                break
 
 quiz_launcher = QuizLauncher()
 quiz_launcher.run()
