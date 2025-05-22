416a63805f96f753e6647eb90873a97295bf82e1
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-05-23 02:24:26 +0800

    fixed_error_allowing_it_to_work

--- quiz_launcher.py
+++ quiz_launcher.py
@@ -1,25 +1,19 @@
-from quiz_creator_oop import Filename, FileMaker, QuestionParts, QuestionWriter, ContinueOrQuit
+from quiz_creator_oop import Filename, FileMaker, QuestionParts, QuestionWriter
 
 class QuizLauncher(Filename, FileMaker, QuestionParts, QuestionWriter):
-    def __init__(self):
+ def __init__(self):
         Filename.__init__(self)
-        FileMaker.__init__(self)
-        self.set_file_name() 
-        self.filename(self)  
-        self.make_file()   
+        self.set_file_name()
+        self.make_file(self.file_name)
+        QuestionWriter.__init__(self, self.file_name)   
 
-
-def run(self):
+ def run(self):
         while True:
             question = self.ask_question()
             answers = self.get_answers()
             correct_answer = self.get_correct_answer()
             self.save_question_to_file(question, answers, correct_answer)
-
-            continue_or_quit = ContinueOrQuit()
-            if continue_or_quit.another_question.lower() != "y":
-                print("Exiting the program.")
-                break
+            
 
 quiz_launcher = QuizLauncher()
 quiz_launcher.run()
