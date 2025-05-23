0354f8fb42faa2e87b99f3c00d1368274ea4ddd4
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-05-23 12:02:43 +0800

    added_check_to_see_if_file_exists

--- quiz_answer_oop.py
+++ quiz_answer_oop.py
@@ -4,3 +4,11 @@ import random
 class Filename:
     def set_file_name(self, file_name="quiz.txt"):
         self.file_name = file_name
+
+    def check_if_file_exists(self):
+        if os.path.exists(self.file_name):
+            print("Quiz file exists")
+            return True
+        else:
+            print("Quiz file does not exist. Please make one")
+            return False
